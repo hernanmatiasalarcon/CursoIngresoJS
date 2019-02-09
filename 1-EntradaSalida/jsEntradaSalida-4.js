72c79cd99e9608cf495f08cd8229a6a9259fa767
hernanmatiasalarcon <hernanalarconsf@gmail.com>
2019-02-08 21:37:43 -0300

    mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -1,9 +1,16 @@
 /* 
 	Debemos lograr tomar un dato por 'PROMPT' 
-	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar' 
 */
 function mostrar()
 {
+    var nombre, elNombre;
+
+    nombre=prompt("ingrese su nombre");
+
+    nombre=document.getElementById("elNombre").value;
+    
+
 
 }
 
